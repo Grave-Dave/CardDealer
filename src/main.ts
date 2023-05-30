80cc5e8fd81aef6aed5b1a7256b1a276122bf05b
DGrabarz <Dawid.Grabarz@hemmersbach.com>
2023-05-30 16:36:11 +0200

    another improvements

--- src/main.ts
+++ src/main.ts
@@ -7,78 +7,56 @@ let deckId = 0
 const Figure: string[] = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
 export default class CardDealer {
-    private readonly deckToDeal: number
+    private readonly cardsToDeal: number
     private joker: boolean
+    public deck :CardDeck
 
-    constructor(deck: number, joker: boolean) {
-        this.deckToDeal = deck
+    constructor(cards: number, joker: boolean) {
+        this.cardsToDeal = cards
         this.joker = joker
+        this.deck = this.createDeck()
     }
 
-    public getRandomCard(): Card[] {
+    public createDeck() {
+        return {
+            id: deckId,
+            data: this.joker
+                ? new Array(54).fill(0).map((el: number, i: number): Card => ({
+                    id: i,
+                    figure: i < 52 ? Figure[i % 13] : 'Joker',
+                    color: i <= 12 ? 'Clubs' : i <= 25 ? 'Diamonds' : i <= 38 ? 'Hearts' : i <= 51 ? 'Spades' : 'Joker'
+                }))
+                : new Array(52).fill(0).map((el: number, i: number): Card => ({
+                    id: i,
+                    figure: Figure[i % 13],
+                    color: i <= 12 ? 'Clubs' : i <= 25 ? 'Diamonds' : i <= 38 ? 'Hearts' : 'Spades'
+                }))
+        }
+    }
+    public getRandomCard(): Card {
         let cardToDeal: Card
-        const randomNum: number = Math.floor(Math.random() * cardDeck.data.length)
-        cardToDeal = cardDeck.data[randomNum]
+        const randomNum: number = Math.floor(Math.random() * this.deck.data.length)
+        cardToDeal = this.deck.data[randomNum]
         removeCard([cardToDeal])
-        return [cardToDeal]
+        return cardToDeal
     }
 
     public getMultipleCard(): Card[] {
         const cardArray: number[] = []
         let cardsToDeal: Card[]
-        while (cardArray.length < this.deckToDeal) {
-            const randomNum: number = Math.floor(Math.random() * cardDeck.data.length)
+        while (cardArray.length < this.cardsToDeal) {
+            const randomNum: number = Math.floor(Math.random() * this.deck.data.length)
             if (cardArray.indexOf(randomNum) === -1) cardArray.push(randomNum);
         }
-        cardsToDeal = cardArray.map(el => cardDeck.data[el])
+        cardsToDeal = cardArray.map(el => this.deck.data[el])
         removeCard(cardsToDeal)
         return cardsToDeal
     }
 
-    public checkJoker(): boolean {
-        return this.joker
-    }
-}
-
-const cards = new CardDealer(10, false)
-
-export const cardDeck: CardDeck = {
-    id: deckId,
-    data: cards.checkJoker()
-        ? new Array(54).fill(0).map((el: number, i: number): Card => ({
-            id: i,
-            figure: i < 52 ? Figure[i % 13] : 'Joker',
-            color: i <= 12 ? 'Clubs' : i <= 25 ? 'Diamonds' : i <= 38 ? 'Hearts' : i <= 51 ? 'Spades' : 'Joker'
-        }))
-        : new Array(52).fill(0).map((el: number, i: number): Card => ({
-            id: i,
-            figure: Figure[i % 13],
-            color: i <= 12 ? 'Clubs' : i <= 25 ? 'Diamonds' : i <= 38 ? 'Hearts' : 'Spades'
-        }))
 
 }
+export const cards = new CardDealer(52, false)
 
-let cardToDisplay : string[]
-let cardsToDisplay : string[]
-
-function displayCard() {
-    cardToDisplay = cards.getRandomCard().map(el => `<div class="card">${el.figure}</div>`)
-    render()
-}
-function displayCards() {
-    cardsToDisplay = cards.getMultipleCard().map(el => `<div class="card">${el.figure}</div>`)
-    render()
-}
-
-document.querySelector('.random').addEventListener('click', displayCard)
-document.querySelector('.multiple').addEventListener('click', displayCards)
-
-function render(){
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="box">  
-        ${cardToDisplay ? cardToDisplay : cardsToDisplay ? cardsToDisplay : 'choose card'}       
-      </div>
-`
-}
+console.log(cards.getMultipleCard())
+console.log(cards.getRandomCard())
 
